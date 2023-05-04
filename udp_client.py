b4dff1352bf88ac6a6c07815961ea6eeaca31959
amartin4 <aamartin20@students.desu.edu>
2023-05-04 17:34:54 -0400

    Finished Server and Client

--- udp_client.py
+++ udp_client.py
@@ -3,7 +3,6 @@ import hashlib  # needed to calculate the SHA256 hash of the file
 import sys  # needed to get cmd line parameters
 import os.path as path  # needed to get size of file in bytes
 
-
 IP = '127.0.0.1'  # change to the IP address of the server
 PORT = 12000  # change to a desired port number
 BUFFER_SIZE = 1024  # change to a desired buffer size
@@ -21,13 +20,13 @@ def get_file_size(file_name: str) -> int:
 
 def send_file(filename: str):
     # get the file size in bytes
-    # TODO: section 2 step 2 in README.md file
+    filesize = get_file_size(filename)
 
     # convert the file size to an 8-byte byte string using big endian
-    # TODO: section 2 step 3 in README.md file
+    size = (filesize).to_bytes(8, byteorder='big')
 
     # create a SHA256 object to generate hash of file
-    # TODO: section 2 step 4 in README.md file
+    file_hash = hashlib.sha256()
 
     # create a UDP socket
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -35,23 +34,33 @@ def send_file(filename: str):
     try:
         # send the file size in the first 8-bytes followed by the bytes
         # for the file name to server at (IP, PORT)
-        # TODO: section 2 step 6 in README.md file
+        client_socket.sendto(size + filename.encode(), (IP, PORT))
+        response, server_address = client_socket.recvfrom(BUFFER_SIZE)
 
-        # TODO: section 2 step 7 in README.md file
+        if response != b'go ahead':
+            raise Exception('Bad server response - was not go ahead!')
 
         # open the file to be transferred
         with open(file_name, 'rb') as file:
             # read the file in chunks and send each chunk to the server
-            # TODO: section 2 step 8 a-d in README.md file
-            pass  # replace this line with your code
+
+            while len(chunk) > 0:
+                file_hash.update(chunk)
+                client_socket.sendto(chunk, (IP, PORT))
+                chunk = file.read(BUFFER_SIZE)
+                response_1, server_address = client_socket.recvfrom(BUFFER_SIZE)
+                if response_1 != b'received':
+                    raise Exception('Bad server response - was not go ahead!')
 
         # send the hash value so server can verify that the file was
         # received correctly.
-        # TODO: section 2 step 9 in README.md file
+        hash_calculated = (file_hash.digest())
+        response_2, server_address = client_socket.sendto(hash_calculated, (IP, PORT))
 
-        # TODO: section 2 steps 10 in README.md file
-
-        # TODO: section 2 step 11 in README.md file
+        if response_2 == b'failed':
+            raise Exception('Transfer failed!')
+        else:
+            print('Transfer completed!')
     except Exception as e:
         print(f'An error occurred while sending the file: {e}')
     finally:
--- udp_server.py
+++ udp_server.py
@@ -14,16 +14,31 @@ def get_file_info(data: bytes) -> (str, int):
 
 def upload_file(server_socket: socket, file_name: str, file_size: int):
     # create a SHA256 object to verify file hash
-    # TODO: section 1 step 5 in README.md file
+    file_verify = hashlib.sha256()
 
     # create a new file to store the received data
     with open(file_name+'.temp', 'wb') as file:
-        # TODO: section 1 step 7a - 7e in README.md file
-        pass  # replace this line with your code for section 1 step 7a - 7e
+        byte_size = 0
+        while True:
+            chunk, client_address = server_socket.recvfrom(BUFFER_SIZE)
+            byte_size += len(chunk)
+            file.write(chunk)
+            file_verify.update(chunk)
+            server_socket.sendto(b'received', client_address)
+            if file_verify == file_size:
+                break
 
     # get hash from client to verify
-    # TODO: section 1 step 8 in README.md file
-    # TODO: section 1 step 9 in README.md file
+    hash_calculated = (file_verify.digest())
+
+    response_3, server_address = server_socket.recvfrom(BUFFER_SIZE)
+
+    if response_3 == hash_calculated:
+        server_socket.sendto(b'success', client_address)
+    else:
+        server_socket.sendto(b'failed', client_address)
+
+
 
 
 def start_server():
@@ -34,10 +49,10 @@ def start_server():
 
     try:
         while True:
-            # TODO: section 1 step 2 in README.md file
+            message, client_address = server_socket.recvfrom(BUFFER_SIZE)
             # expecting an 8-byte byte string for file size followed by file name
-            # TODO: section 1 step 3 in README.md file
-            # TODO: section 1 step 4 in README.md file
+            file_name, file_size = get_file_info(message)
+            server_socket.sendto(b'go head', client_address)
             upload_file(server_socket, file_name, file_size)
     except KeyboardInterrupt as ki:
         pass
